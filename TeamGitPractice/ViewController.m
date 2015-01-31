5e2e4096d06068a8e383b8748521e3eb67a51fb4
Chris Giersch <chgiersch@gmail.com>
2015-01-30 17:36:00 -0800

    cleared everything

--- TeamGitPractice/ViewController.m
+++ TeamGitPractice/ViewController.m
@@ -16,26 +16,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    // Hey guys, Let's talk through here
-    NSLog(@"Okay - Irwin");
-    NSLog(@"Cool Beans");
-    
-    //Mic check 1 2 1 2
-
-    NSLog(@"I have the rubber duck, I make the rules. :P - Yi-Chin");
-
-    // Hey, I'm talking here...HAPPY!!?
-    NSLog(@"Talk, talk, talk.");
-
-    NSLog(@"I have the rubber duck, I make the rules. :P - Yi-Chin");
-
-    // Hey, I'm talking here...HAPPY!!?
-    NSLog(@"Talk, talk, talk.");
-    
-    // Hi
-    NSLog(@"Food");
-
 }
 
 - (void)didReceiveMemoryWarning {
