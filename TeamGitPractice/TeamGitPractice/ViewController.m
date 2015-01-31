e0af927de2e02af926922707e53ed9ec7dccc270
mewpre <yichin.sun@gmail.com>
2015-01-30 17:34:39 -0800

    added comment

---
+++ TeamGitPractice/TeamGitPractice/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TeamGitPractice
+//
+//  Created by Irwin Gonzales on 1/30/15.
+//  Copyright (c) 2015 Irwin Gonzales. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Hey guys, Let's talk through here
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
