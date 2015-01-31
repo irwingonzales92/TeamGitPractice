772c13bf2ca4b56988f7cfb77374ba446d274cb8
irwingonzales92 <irwin@newdesto.com>
2015-01-30 18:05:27 -0800

    Deleted Weird Stuff

--- TeamGitPractice/ViewController.m
+++ TeamGitPractice/ViewController.m
@@ -18,32 +18,6 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-<<<<<<< HEAD
-    
-    // Hey guys, Let's talk through here
-    NSLog(@"Okay - Irwin");
-    NSLog(@"Cool Beans");
-    
-    //Mic check 1 2 1 2
-
-    NSLog(@"I have the rubber duck, I make the rules. :P - Yi-Chin");
-
-    // Hey, I'm talking here...HAPPY!!?
-    NSLog(@"Talk, talk, talk.");
-    
-    // Hi
-    NSLog(@"Food");
-    
-    // Bye
-    NSLog(@"K");
-
-    NSLog(@"Let's get some merge conflicts all up in this biz.");
-
-
-
-=======
-    //Change label text here please!
->>>>>>> a863e96f4f07eb601329561b4ebb2250b7a87af3
 }
 
 - (void)didReceiveMemoryWarning {
